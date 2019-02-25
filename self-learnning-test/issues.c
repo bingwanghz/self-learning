d97e54d6881a86c4cfb1e1691154cd62252965ff
Bing Wang <binghz.wang@gmail.com>
2019-02-25 09:15:07 -0500

    Q8: char x (-128--127)
    Signed-off-by: Bing Wang <binghz.wang@gmail.com>

--- self-learnning-test/issues.c
+++ self-learnning-test/issues.c
@@ -8,6 +8,7 @@ int main()
 	int a[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
 	int *p = &a[1];
 	int i;
+	int d;
 
 	printf("p[6] = %d\n", p[6]);
 	printf("sizeof(a) = %lu\n", sizeof(a));
@@ -22,6 +23,15 @@ int main()
 	char *t;
 	t = (str+4)[-1];
 	printf("t = %s\n", t);
+
+	char c = 0x80;
+	d = c;
+	printf("d = %d\n", d);
+	if ( i < 0) {
+		printf("return 1\n");
+	} else {
+		printf("return 2\n");
+	}
 
 	printf("executiong successfully\n");
 
