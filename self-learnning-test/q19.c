160ec38cf1152dc8ab8eca65ac76c98bef62a561
Bing Wang <binghz.wang@gmail.com>
2019-02-27 09:09:02 -0500

    Add question 19
    Signed-off-by: Bing Wang <binghz.wang@gmail.com>

---
+++ self-learnning-test/q19.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	int j = 2;
+	int i = 1;
+	if (i = 1) {
+		j = 3 ;
+		printf("enter i = 1 so j = 3 \n");
+	}
+	if (i = 2) {
+		j = 5;
+		printf("enter i= 2 so j =5\n");
+
+	}
+	printf("j = %d\n", j);
+}
